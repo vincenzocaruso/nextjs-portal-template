f9007984d9e850f378bfd00be225d0182b93e167
Vincenzo Caruso <vicaruso@microsoft.com>
2023-03-22 12:51:05 -0700

    add homepage with tech stack link

---
+++ src/components/listItem.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import styles from './listItem.module.scss';
+import classNames from 'classnames/bind';
+import Image from 'next/image';
+
+const cx = classNames.bind(styles);
+
+export default function ListItem({ href, name, description, icon }: ListItemProps) {
+    return (
+        <a href={href} title='name' target='_blank' rel="noreferrer">
+            <div className={cx('container')}>
+                <span className={cx('icon')}>
+                    <Image src={icon} alt={`${name}-logo`} width={64} height={64} />
+                </span>
+                <div className={cx('text-container')}>
+                    <p className={cx('name')}>{name}</p>
+                    <p className={cx('description')}>{description}</p>
+                </div>
+            </div>
+        </a>
+    );
+}
+
+export interface ListItemProps {
+    name: string;
+    href: string;
+    description: string;
+    icon: string;
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,11 +6,46 @@ import Head from 'next/head';
 import { useTranslation } from 'next-i18next';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import classNames from 'classnames/bind';
+import type { ListItemProps } from '../components/listItem';
+import ListItem from '../components/listItem';
 
 const cx = classNames.bind(styles);
 
 const Home: NextPage = (_props: InferGetServerSidePropsType<typeof getServerSideProps>) => {
     const { t } = useTranslation();
+
+    const techStack = React.useMemo<ListItemProps[]>(() => [
+        {
+            name: 'Next.js',
+            description: 'React Framework',
+            href: 'https://nextjs.org/',
+            icon: '/assets/next-logo.png'
+        },
+        {
+            name: 'Next.js',
+            description: 'React Framework',
+            href: 'https://nextjs.org/',
+            icon: ''
+        },
+        {
+            name: 'Next.js',
+            description: 'React Framework',
+            href: 'https://nextjs.org/',
+            icon: ''
+        },
+        {
+            name: 'Next.js',
+            description: 'React Framework',
+            href: 'https://nextjs.org/',
+            icon: ''
+        },
+        {
+            name: 'Next.js',
+            description: 'React Framework',
+            href: 'https://nextjs.org/',
+            icon: ''
+        }
+    ], []);
 
     return (
         <>
@@ -22,6 +57,13 @@ const Home: NextPage = (_props: InferGetServerSidePropsType<typeof getServerSide
             <div className={cx('main')}>
                 <h1 className={cx('title')}>{t('UXPortal')}</h1>
                 <p>{t('Welcome')}</p>
+                <div className={cx('content')}>
+                    <h2>{t('TechStackTitle')}</h2>
+                    <span>{t('TechStackDescription')}</span>
+                    <div className={cx('tech-stack-list')}>
+                        {techStack.map((item, i) => <ListItem key={i} {...item} />)}
+                    </div>
+                </div>
             </div>
         </>
     );
