a1a4b4ca1b8107a87eab745554cb6ba08e453872
vincenzocaruso <vicaruso@microsoft.com>
2024-08-05 21:12:01 -0700

    remove wrong import

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,4 @@
 import 'sanitize.css';
-import '@/styles/globals.scss';
-import '@/styles/colors.scss';
 
 import type { AppProps, AppType } from 'next/app';
 import type { GriffelRenderer } from '@fluentui/react-components';
